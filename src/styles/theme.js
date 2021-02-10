428f75c0dd63fe1f8a5670a11fe2697d8d33c09f
Nikolas Barwicki <nikolas.barwicki@gmail.com>
2021-02-10 20:45:05 +0100

    style categories section

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -5,10 +5,12 @@ export const theme = {
     secondary: "#55527C",
     background: "#F7F6FB",
     lightText: "#B4B2C3",
+    borderActive: "#F8979B",
     backgroundPurple: "#F3F3FC",
     backgroundBlue: "#E9F9FF",
   },
   shadow: {
     card: "1px 1px 118px 150px rgba(206,212,224,0.18)",
+    category: "14px 14px 120px 0 rgba(206,212,224,0.18)",
   },
 }
